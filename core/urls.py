6da9276bd3f83d6f483059d0bcfd615607a4b447
hasan <hasanocal7@gmail.com>
2024-02-27 14:43:14 +0300

    Add GraphQLView

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from strawberry.django.views import AsyncGraphQLView
+from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', AsyncGraphQLView.as_view(schema=schema))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
