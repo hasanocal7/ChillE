a1660ddf4bfb46f76f496855244897ea1b95f2c7
hasan <hasanocal7@gmail.com>
2024-03-01 15:21:34 +0300

    Update UserAdmin Model for Updated Auth Model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Account
+from .models import CustomUser
 
-@admin.register(Account)
-class AccountAdmin(admin.ModelAdmin): pass
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin): pass
